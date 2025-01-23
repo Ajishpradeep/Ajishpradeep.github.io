98a43af2c57e2be1aa987ed2d01cce04ada11ec6
Ajish Pradeep <pradeepkrishna94@gmail.com>
2025-01-23 13:15:20 +0800

    new js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 'use strict';
 
+// Element toggle function
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
-
-// element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
-
-// sidebar variables
+// Sidebar toggle functionality for mobile
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
+
+// SERVICE MODAL LOGIC
 
 // Define service data (full descriptions)
 const serviceDetails = [
@@ -26,164 +27,60 @@ const serviceDetails = [
   },
 ];
 
-// Open modal
+// Modal selectors
+const serviceItems = document.querySelectorAll("[data-service-item]");
+const serviceModalContainer = document.querySelector("[data-modal-container]");
+const serviceModalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const serviceModalOverlay = document.querySelector("[data-overlay]");
+const serviceModalTitle = document.querySelector("[data-modal-title]");
+const serviceModalText = document.querySelector("[data-modal-text]");
+
+// Open modal for services
 serviceItems.forEach((item, index) => {
-  const popupBtn = item.querySelector('[data-popup-btn]');
-  popupBtn.addEventListener('click', () => {
-    modalTitle.textContent = serviceDetails[index].title;
-    modalText.textContent = serviceDetails[index].text;
-    modalContainer.removeAttribute('hidden');
+  const popupBtn = item.querySelector("[data-popup-btn]");
+  popupBtn.addEventListener("click", () => {
+    serviceModalTitle.textContent = serviceDetails[index].title;
+    serviceModalText.textContent = serviceDetails[index].text;
+    serviceModalContainer.removeAttribute("hidden");
+    serviceModalContainer.classList.add("active");
   });
 });
 
-// Close modal
-modalCloseBtn.addEventListener('click', () => {
-  modalContainer.setAttribute('hidden', '');
-});
+// Close modal for services
+const closeServiceModal = () => {
+  serviceModalContainer.setAttribute("hidden", "");
+  serviceModalContainer.classList.remove("active");
+};
 
-modalOverlay.addEventListener('click', () => {
-  modalContainer.setAttribute('hidden', '');
-});
+serviceModalCloseBtn.addEventListener("click", closeServiceModal);
+serviceModalOverlay.addEventListener("click", closeServiceModal);
+
+// TESTIMONIALS MODAL LOGIC
+
+const testimonialsItems = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector('.service-modal-container');
+const modalOverlay = document.querySelector('.service-overlay');
 
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
-
-// modal variable
-const modalImg = document.querySelector("[data-modal-img]");
-const modalTitle = document.querySelector("[data-modal-title]");
-const modalText = document.querySelector("[data-modal-text]");
-
-// modal toggle function
+// Modal toggle function
 const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
+  serviceModalContainer.classList.toggle("active");
+  serviceModalOverlay.classList.toggle("active");
+};
 
-// add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
-
-  testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+// Add click event to all testimonial items
+testimonialsItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    const modalImg = document.querySelector("[data-modal-img]");
+    modalImg.src = item.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = item.querySelector("[data-testimonials-avatar]").alt;
+    serviceModalTitle.innerHTML = item.querySelector("[data-testimonials-title]").innerHTML;
+    serviceModalText.innerHTML = item.querySelector("[data-testimonials-text]").innerHTML;
 
     testimonialsModalFunc();
-
   });
+});
 
-}
-
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
-
-
-
-// custom select variables
-const select = document.querySelector("[data-select]");
-const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
-
-select.addEventListener("click", function () { elementToggleFunc(this); });
-
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-
-  });
-}
-
-// filter variables
-const filterItems = document.querySelectorAll("[data-filter-item]");
-
-const filterFunc = function (selectedValue) {
-
-  for (let i = 0; i < filterItems.length; i++) {
-
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
-    }
-
-  }
-
-}
-
-// add event in all filter button items for large screen
-let lastClickedBtn = filterBtn[0];
-
-for (let i = 0; i < filterBtn.length; i++) {
-
-  filterBtn[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
-
-  });
-
-}
-
-
-
-// contact form variables
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
-
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
-
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-
-  });
-}
-
-
-
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
-
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
-
-  });
-}
+// Add click event to modal close button
+serviceModalCloseBtn.addEventListener("click", testimonialsModalFunc);
+serviceModalOverlay.addEventListener("click", testimonialsModalFunc);
