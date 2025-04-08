0e057660dae2fc76e26c76aa7aef82d30ae2be36
Ajishpradeep <pradeepkrishna94@gmail.com>
2025-04-08 15:00:46 +0800

    optimized mobile view & Added animations

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+// Simple service worker for basic offline support
+const CACHE_NAME = "ajish-portfolio-cache-v1"
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/profile_pic.png",
+  "/resume.pdf",
+  "/manifest.json",
+]
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)))
+})
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)))
+})
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME]
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
+        }),
+      )
+    }),
+  )
+})
